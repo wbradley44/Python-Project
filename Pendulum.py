df8c7430e16dd4f774d32e42615e0ad35f8ca673
ariellongoria <60207588+ariellongoria@users.noreply.github.com>
2020-05-05 15:10:01 -0500

    Commented; added controller simulation start/stop

--- Pendulum.py
+++ Pendulum.py
@@ -15,6 +15,8 @@ y2 = y1 - l2 cos(theta2)
 alpha = ddt omega = ddt ddt theta
 """
 pg.init()
+
+# Pendulum class stores values of angular displacement, velocity, and acceleration, as well as length and mass of the pendulum.
 class pend:
     def __init__(self, m, l, theta1 = 0, l1 = 0):
         self.m = m
@@ -28,6 +30,7 @@ class pend:
 top = pend(1,1)  
 bottom = pend(1,1,theta1 = 0, l1 = 1)      
 
+#updates the positions and velocities of the pendulums.
 def update(up, dn,t0,t1,tstep):
     #integrate both for the timestep
     #take in initial conditions
@@ -41,6 +44,7 @@ def update(up, dn,t0,t1,tstep):
     dn.theta = sol[-1,2]
     dn.omega = sol[-1,3]
 
+#function is used in the update function to solve for the displacement and velocity values
 def omegadot(y, t, m1, l1, m2, l2):
     # must be in this form for scipy.integrate.odeint
     g = 9.81
@@ -50,6 +54,7 @@ def omegadot(y, t, m1, l1, m2, l2):
     #equation defining the angular acceleration of the top pendulum
     return alpha
 
+#transforms the data to coordinates we can use in pygame
 def findXY(up,dn):
     scrnsz = int(400*(top.l+bottom.l))
     middlex = int(scrnsz/2)
@@ -59,6 +64,7 @@ def findXY(up,dn):
     dn.y = middlex - int(np.round(200*(-dn.l * cos(dn.theta) - cos(up.theta) * up.l)))
     return
 
+#redraws the elements on the screen
 def redraw(up,dn):
     screen.fill((255,255,255))
     pg.draw.line(screen,(0,0,0),(400, 400),(up.x,up.y),10)
@@ -76,15 +82,23 @@ t1 = .03
 tstep =  .005
 screen.fill((255,255,255))
 
+#initializes joystick, only takes the first joystick it detects
 pg.joystick.init()
 try:
     gamepad = pg.joystick.Joystick(0)
     gamepad.init()
 except:
     print("No joystick detected, I guess you're not a gamer")
+    
+#main loop
 while run: 
     #os.system('cls')
     #Quit when 'x' is pressed if there is a pygame window open.
+    #up arrow sets both pendulums vertically
+    #down arrow sets a random position
+    #right arrow sets both pendulums to the right
+    #left arrow sets both pendulums to the left
+    #space starts/stops the simulation
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
@@ -115,11 +129,16 @@ while run:
                 run = False
                 top.omega = 0
                 bottom.omega = 0
+    #right joystick moves first pendulum around
+    #left joystick moves second pendulum around
+    #'X' button starts/stops simulation
     try:
         if abs(gamepad.get_axis(2)) > 0.2:
             top.theta += gamepad.get_axis(2)*np.pi/20
         elif abs(gamepad.get_axis(0)) > 0.2:
             bottom.theta +=gamepad.get_axis(0)*np.pi/20
+        elif gamepad.get_button(1):
+            drop = not drop
     except:
         pg.display.flip()
     if drop:
